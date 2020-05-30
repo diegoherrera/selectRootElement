f969ea088bf079d77373f4f911951e84a5ce823e
Diego Sebastian Herrera <diherrer@everis.com>
2020-05-30 12:02:15 -0300

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  @ViewChild('contenedorPadre')
+  private contenedorPadre: ElementRef;
+
+  @ViewChild('contenedorHijo1')
+  private contenedorHijo1: ElementRef;
+
   title = 'render2';
+  constructor(private rendered: Renderer2) {     
+  }
+
+  onClickEvent() {
+    let selector = this.rendered.selectRootElement(
+      this.contenedorPadre.nativeElement);
+    const texto = this.rendered.createText("Hola mundo");
+    this.rendered.appendChild(selector, texto);
+  }
 }
